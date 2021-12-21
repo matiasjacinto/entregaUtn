4a80d9f200e2d77336de113c641005a9eb318769
Matias Jacinto <matymdq2015@gmail.com>
2021-12-21 10:37:47 -0300

    modificar novedades

--- models/novedadesModels.js
+++ models/novedadesModels.js
@@ -21,4 +21,26 @@ async function deleteNovedadByID(id) {
   return rows;
 }
 
-module.exports = { getNovedades, insertAdmiNovedad, deleteNovedadByID };
+async function getNovedadesByID(id) {
+  var query = " select * from adminovedades where id=?";
+  var rows = await pool.query(query, [id]);
+  return rows[0];
+}
+
+async function modificarNovedadByID(obj, id) {
+  try {
+    var query = "update adminovedades set? where id=?";
+    var rows = await pool.query(query, [obj, id]);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  getNovedades,
+  insertAdmiNovedad,
+  deleteNovedadByID,
+  getNovedadesByID,
+  modificarNovedadByID,
+};
