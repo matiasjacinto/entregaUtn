231ca8d256c3d6b528352c4aba632f9ce0c6f42f
Matias Jacinto <matymdq2015@gmail.com>
2021-12-22 03:18:16 -0300

    fin imagenes base de adtos, cloudbinary

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -4,6 +4,7 @@ var novedadesModel = require("../../models/novedadesModels");
 var util = require("util");
 var cloudinary = require("cloudinary").v2;
 const uploader = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
 
 /* listado de novedades*/
 router.get("/", async function (req, res, next) {
@@ -82,6 +83,11 @@ router.post("/agregar", async function (req, res, next) {
 
 router.get("/eliminar/:id", async function (req, res, next) {
   var id = req.params.id;
+  let novedad = await novedadesModel.getNovedadesByID(id);
+
+  if (novedad.img_id) {
+    await destroy(novedad.img_id);
+  }
   await novedadesModel.deleteNovedadByID(id);
   res.redirect("/admin/novedades");
 });
@@ -97,10 +103,26 @@ router.get("/modificar/:id", async (req, res, next) => {
 
 router.post("/modificar", async (req, res, next) => {
   try {
+    let img_id = req.body.img_original;
+    let borrar_img_vieja = false;
+    if (req.body.img_delete === "1") {
+      img_id = null;
+      borrar_img_vieja = true;
+    } else {
+      if (req.files && Object.keys(req.files).length > 0) {
+        imagen = req.files.imagen;
+        img_id = (await uploader(imagen.tempFilePath)).public_id;
+        borrar_img_vieja = true;
+      }
+    }
+    if (borrar_img_vieja && req.body.img_original) {
+      await destroy(req.body.img_original);
+    }
     var obj = {
       titulo: req.body.titulo,
       subtitulo: req.body.subtitulo,
       cuerpo: req.body.cuerpo,
+      img_id,
     };
     await novedadesModel.modificarNovedadByID(obj, req.body.id);
     res.redirect("/admin/novedades");
